e590dcae60c5fde5900c30ac8a1b54c657dee4f2
JuanReynoB <juanreynob@gmail.com>
2022-08-16 13:35:03 -0300

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+const persona = {nombre: "JR", proyecto: "JR Shoes"};
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bievenidos a {persona.proyecto}
         </p>
+        <ButtonComponent/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +23,10 @@ function App() {
       </header>
     </div>
   );
+}
+
+const ButtonComponent = () => {
+  return <button>Click para ingresar!</button>
 }
 
 export default App;
