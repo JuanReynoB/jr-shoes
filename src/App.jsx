6964cb7582bc88f8a4bfc9ba1321df5e6558322f
JuanReynoB <juanreynob@gmail.com>
2022-08-22 22:06:30 -0300

    JR shoes - Juan Agustin Reyno Buch

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import './components/Navbar/navbar.css';
+
 
 const App = () => {
 const persona = {nombre: "JR", proyecto: "JR Shoes"};
 
   return (
     <div className="App">
+    <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Bievenidos a {persona.proyecto}
         </p>
-        <ButtonComponent/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
-}
-
-const ButtonComponent = () => {
-  return <button>Click para ingresar!</button>
 }
 
 export default App;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,18 @@
+
+
+const Navbar = () =>{
+    return (
+       <header className='header'>
+            <h1 className='header-brand-name'>JR Shoes</h1>
+            <nav className='header-nav'>
+                <ul>
+                    <li><a href=''>MODELOS</a></li>
+                    <li><a href=''>NOSOTROS</a></li>
+                    <li><a href=''>CONTACTO</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
